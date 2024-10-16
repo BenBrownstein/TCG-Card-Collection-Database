fb4d8259aa3100c795eaf312f97724fc4f6e2fa0
Ben Brownstein <82259256+BenBrownstein@users.noreply.github.com>
2024-10-15 17:55:14 -0700

    Add files via upload
    Added some PhP files that will show the cards in the database

---
+++ index.php
@@ -0,0 +1,67 @@
+<h1>These are the Magic Cards that have been added to the Database</h1>
+
+
+
+<?php
+
+  $servername = "localhost";
+
+  $username   = "root";
+
+  $password   = "";
+
+  $dbname     = "card_collection";
+
+  
+
+  // Create connection object
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+  // Check connection
+
+  if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+  }
+
+  
+
+  $sql = "SELECT c.name, mc.rarity, mc.artist, mc.image_url, mc.is_Legendary, mcct.card_type
+  FROM cards c JOIN magic_cards mc ON c.card_id = mc.card_id
+  JOIN magic_cards_card_types_relations mcctr ON mcctr.magic_card_id = mc.magic_card_id
+  JOIN magic_card_card_types mcct ON mcct.magic_card_card_type_id = mcctr.magic_card_card_type_id";
+  
+
+
+  $result = $conn->query($sql);
+
+  
+
+  if ($result->num_rows > 0) {
+
+    // output data of each row
+
+    while($row = $result->fetch_assoc()) {
+
+      echo "Name: " . $row["name"]. "<br>". " Card Type: ". $row["card_type"]. "<br>". "Image: ". "<img src=". $row["image_url"]. "/>". "<br>";
+
+    }
+
+  } else {
+
+    echo "0 results";
+
+  }
+
+  $conn->close();
+
+
+  
+
+
+
+?>
